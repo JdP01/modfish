36340a97193bea52f7a7124b3c4f3a04cb7335bb
Juan Pleitez <juandipleitez04@gmail.com>
2026-09-22 21:05:53 -0600

    added hexadecimal representation for all piece locations in bitboard

--- src/main.rs
+++ src/main.rs
@@ -16,26 +16,56 @@ struct Side{
 
 fn main() {
     
-    let white_board = Boards{pawns:0xFF00,
-        rooks:0x8100000000000000,
-        bishops:0,
-        knights:0,
-        king:0,
-        queen:0};
-    let white = Side{color: true,pieces: white_board};
+    let white_board = Boards{pawns:0xFF00, //board starting positions for white
+        rooks:0x81,
+        bishops:0x42,
+        knights:0x24,
+        king:0x8,
+        queen:0x10
+    };
+    let white = Side{color: true,pieces: white_board}; //team color 
 
-    let black_board = Boards{pawns:0xFF000000000000,
-        rooks:0x1000000000000000,
-        bishops:0,
-        knights:0,
-        king:0,
-        queen:0};
+
+    let black_board = Boards{pawns:0xFF000000000000, //same for black
+        rooks:0x8100000000000000,
+        bishops:0x4200000000000000,
+        knights:0x2400000000000000,
+        king:0x800000000000000,
+        queen:0x1000000000000000
+    };
     let black = Side{color: true,pieces: black_board};
 
-    println!("{:064b}",white.pieces.pawns);
-    println!("{:064b}",white.pieces.rooks);
-    println!("{:064b}",black.pieces.pawns);
+    //let q = white.pieces.queen;
+    //let k = white.pieces.king;
+    //let kn = white.pieces.knights;
+    //let b = white.pieces.bishops;
+    //let r = white.pieces.rooks;
+    //let p = white.pieces.pawns;
 
+    //print white chec
+
+    let q = black.pieces.queen;
+    let k = black.pieces.king;
+    let kn = black.pieces.knights;
+    let b = black.pieces.bishops;
+    let r = black.pieces.rooks;
+    let p = black.pieces.pawns;
+
+    println!("{:064b}",p);    
+    println!("{:064b}",q);
+    println!("{:064b}",k);
+    println!("{:064b}",kn);
+    println!("{:064b}",b);
+    println!("{:064b}",r);
     
+    println!("{:064b}",q | k | kn | b | r | p);
 
+ 
+    
+    print_board()
+}
+
+fn print_board()-> () {
+    println!("hello");
+    
 }
